c6fa61a774b3dfc4d3b5ed4f12283c139998cfc3
Noémie <noemie.lehuby@zaclys.net>
2019-08-29 16:40:14 +0200

    simplify packaging

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from .context import transporthours
 from transporthours.main import Main
 import unittest
 
--- tests/test_openinghoursparser.py
+++ tests/test_openinghoursparser.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from .context import transporthours
 from transporthours.openinghoursparser import OpeningHoursParser
 import unittest
 
